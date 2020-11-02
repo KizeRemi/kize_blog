094321d9db0570819cdd9524a02817b5ebda0ccf
Rémi Mavillaz <remimavillaz@MBP-de-Remi.home>
2020-11-02 17:31:35 +0100

    remove projects const

--- pages/projets.js
+++ pages/projets.js
@@ -1,24 +1,7 @@
 import React from 'react'
 import Head from 'next/head'
 import BodyLayout from '../components/Layout/BodyLayout';
-const menuConfig = [
-    {
-      href: '/',
-      label: 'Home',
-    },
-    {
-      href: '/project',
-      label: 'Projects',
-    },
-    {
-      href: '#',
-      label: 'Articles',
-    },
-    {
-      href: '#',
-      label: 'Contact',
-    },
-  ]
+
 const Project = () => (
   <div>
     <Head>
