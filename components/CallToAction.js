8e2dfd93d968f0722eded129c0d7c76ce33d739f
Rémi Mavillaz <remimavillaz@MBP-de-Remi.home>
2021-02-06 17:22:54 +0100

    fix button width in cta

--- components/CallToAction.js
+++ components/CallToAction.js
@@ -11,7 +11,7 @@ const CallToAction = ({ title, subTitle, links }) => (
     <div className="flex md:ml-auto md:mr-0 mx-auto items-center flex-shrink-0 space-x-4">
       {links.map(link => (
         <Link href={link.href}>
-          <a className="border-2 inline-flex py-3 px-5 w-44 rounded-lg items-center hover:bg-gray-200 focus:outline-none">
+          <a className="border-2 inline-flex py-3 px-5 rounded-lg items-center hover:bg-gray-200 focus:outline-none">
             <span className="flex items-start flex-col leading-none uppercase">
               <span className="text-xs text-gray-600 mb-1 font-medium">{link.subLabel}</span>
               <span className="title-font font-bold">{link.label}</span>
